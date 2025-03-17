1ffb8937a4f174b568c44ca81000df340581e819
velicharlagokulkumar <velicharlagokulkumar@gmail.com>
2025-03-17 23:25:31 +0530

    first commit

---
+++ src/components/icons/loader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const IconLoader = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="B" transform="translate(11.000000, 5.000000)">
+        <path
+          d="M49 45 L55 45 L55 55 C52 59 47 62 41 62 C32 62 26 54 26 46 C26 37 32 30 41 30 C48 30 53 33 56 38 L50 38 C48 35 45 33 41 33 C35 33 30 38 30 46 C30 54 35 59 41 59 C45 59 49 57 51 54 L51 50 L41 50 L41 45 Z"
+          fill="currentColor"
+        />
+      </g>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
+    </g>
+  </svg>
+);
+
+export default IconLoader;
